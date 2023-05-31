a097f4a966374241d29dfe3ebc4d8a38ce6861c1
shubham <shubhamrupapara993@gmail.com>
2023-05-31 15:45:50 +0530

    login authentication done

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
+import { loginUser, registerUser } from "../controllers/AuthController.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => { res.send("Auth Route")} )
+router.post('/register', registerUser);
+router.post('/login' , loginUser)
 
 
 export default router;
