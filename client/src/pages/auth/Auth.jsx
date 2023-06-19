882d633e9a59d628f2ed1554fb88ad2a7150ec6e
Shubham Rupapara <99493336+shubhamrupapara@users.noreply.github.com>
2023-06-19 18:14:35 +0530

    final touch

--- client/src/pages/auth/Auth.jsx
+++ client/src/pages/auth/Auth.jsx
@@ -52,7 +52,7 @@ const Auth = () => {
       <div className="a-left">
         <img src={Logo} alt="" />
         <div className="webname">
-          <h1>SHR Media</h1>
+          <h1>VibraLink</h1>
           <h6>Explore the ideas throughout the world</h6>
         </div>
       </div>
